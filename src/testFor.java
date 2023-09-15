c25a8f71e833f83172a5080d1a3a01ebd2425f73
robinia <rosemistery@hotmail.com>
2023-09-15 21:26:19 +0800

    2023.9.15

---
+++ src/testFor.java
@@ -0,0 +1,18 @@
+/*
+for循环
+ */
+
+/*
+求1-10的整数和
+ */
+public class testFor {
+    public static void main(String[] args) {
+
+int sum = 0;
+for(int i = 0; i <= 10; i++){
+    sum += i;
+
+    }
+System.out.println("1-10整数和的值为" + sum);
+    }
+}
