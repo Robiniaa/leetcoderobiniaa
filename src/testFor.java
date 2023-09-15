3629fd85af832bd0fdafabd4044eb73574ac012d
robinia <rosemistery@hotmail.com>
2023-09-15 22:29:35 +0800

    2023.9.15

--- src/testFor.java
+++ src/testFor.java
@@ -2,17 +2,33 @@
 for循环
  */
 
+
+public class testFor {
+    public static void main(String[] args) {
 /*
 求1-10的整数和
  */
-public class testFor {
-    public static void main(String[] args) {
-
 int sum = 0;
 for(int i = 0; i <= 10; i++){
     sum += i;
 
     }
 System.out.println("1-10整数和的值为" + sum);
+
+
+ /*
+ 嵌套for循环
+ 特点：外层循环一次，内层循环要完成遍历。
+  */
+System.out.println("九九乘法表：");
+for(int i=1;i<=9;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            System.out.print(j+"*"+i+"="+j*i+"\t");
+        }
+        System.out.println();
+    }
+
     }
 }
